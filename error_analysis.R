d139e6374ff83625b1def34a834b8244cadff977
Ellen <97767587+ellen-27@users.noreply.github.com>
2023-07-12 12:46:24 -0400

    new pivot table, first stacked bar plot

--- error_analysis.R
+++ error_analysis.R
@@ -1,48 +1,42 @@
-# Script for ...
+# Script for Identifying the types of errors made by citizen scientists in Caterpillars Count!
 
 # Load libraries
 library(dplyr)
 library(tidyr)
 library(stringr)
-
+library(ggplot2)
 
 # Read in raw data
 arthro_sight = read.csv("2023-07-06_ArthropodSighting.csv")
 
-
+# true_counts displays OriginalGroup:UpdatedGroup:number of ID's with that pair
 true_counts = arthro_sight %>%
   filter(PhotoURL != "") %>%
   group_by(OriginalGroup, UpdatedGroup) %>%
   summarize(number = n())
 
-# make table that singles out OriginalGroup (includes total ID of OriginalGroup)
+# total shows total amount of OriginalGroup IDs 
+total_counts = arthro_sight %>%
+  filter(PhotoURL != "") %>%
+  group_by(OriginalGroup) %>%
+  summarize(total_ID = n())
+  
 # use left_join() to compare total with proportion from true_counts
-# divide / error rate ... use mutate()
+error_num = true_counts %>%
+  group_by(OriginalGroup) %>% 
+  left_join(total_counts, true_counts, by = c("OriginalGroup" = "OriginalGroup")) %>%
+  mutate(rate = (number / total_ID) * 100) %>%
+  arrange(OriginalGroup, desc(rate)) #to see total desc rate (ie daddylonglegs are the least erroneously-ID'd arthropods) delete 'OriginalGroup' from arrange()
 
-# part 2: use range()
+only_error_num = error_num %>%
+  filter(OriginalGroup != UpdatedGroup)
+
+pivoted = only_error_num %>%
+  pivot_wider(names_from = UpdatedGroup, values_from = rate)
+
+stacked = ggplot(only_error_num, aes(fill=OriginalGroup, y=rate, x=UpdatedGroup)) + geom_bar(position='stack', stat = 'identity') 
+
+# create plots to visualize data
+# image(matrix), pivot_wider(), etc
 
 
-# true_counts = arthro_sight %>% group_by(OriginalGroup) %>% summarise(trueAnts = ??, trueBeetle = ??, etc)
-
-# ATTEMPTS:
-
-# true_counts = summarise(trueAnts = sum((arthro_sight$UpdatedGroup == "ant"))) 
-#cant sum a conditional... 
-
-# arthro_sight$UpdatedGroup %>% true_counts = summarise(trueAnts = count(("ant"))) 
-#"no applicable method for 'count' applied to class "character"
-# str_count() doesn't work either
-
-# arthro_sight$UpdatedGroup %>% true_counts = summarise(trueAnts = count((if_else(arthro_sight$UpdatedGroup == 'ant', 1, 0))))
-# no applicable method for 'count' applied to an object of class 'c('double', 'numeric')
-
-# if_else(arthro_sight$UpdatedGroup == 'ant', 1, 0)
-# every entry is TRUE when 'ant', FALSE when anything other than 'ant'. How to make the code seek through each point?
-
-********************************************************
-  
-  # use 'summarize' to create new variables that are the *counts* of the different values in the UpdatedGroup column
-  
-  # ie for every record that a CC user said was a 'beetle' we want a "trueBeetles" column which is the number of those records that are actually beetles based on UpdatedGroup, as well as trueAnts, trueTrueBugs, etc. columns.
-  
-  # dont fully understand the "summarize" function
